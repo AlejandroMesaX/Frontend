41daa788470bbfc894de209e55104df8a38e073b
AlejandroMesaX <mesa.10.alejandro@gmail.com>
2026-04-08 21:46:17 -0500

    Despliegue V3

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -6,7 +6,7 @@ export default defineConfig({
   server: {
     proxy: {
       "/api": {
-        target: "http://localhost:8080",
+        target: "https://backend-proyecto-0ccj.onrender.com",
         changeOrigin: true,
       },
     },
