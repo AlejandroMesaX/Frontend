55236b765e6e753412350bb6e008c9a599335d03
AlejandroMesaX <mesa.10.alejandro@gmail.com>
2026-04-09 22:13:43 -0500

    Manejando WS en Produccion

--- frontend/src/realtime/stompCliente.js
+++ frontend/src/realtime/stompCliente.js
@@ -1,18 +1,34 @@
 import { Client } from "@stomp/stompjs";
 
-const WS_URL = import.meta.env.VITE_WS_URL ?? "ws://localhost:8080/ws";
+const WS_URL =
+    import.meta.env.VITE_WS_URL ??
+    (window.location.hostname === "localhost"
+        ? "ws://localhost:8080/ws"
+        : "wss://backend-proyecto-0ccj.onrender.com/ws");
 
 export function createStompClient({ token, onConnect, onError }) {
     const client = new Client({
         brokerURL: `${WS_URL}?token=${token}`,
+
         reconnectDelay: 3000,
         heartbeatIncoming: 10000,
         heartbeatOutgoing: 10000,
+
+        debug: (str) => {
+            console.log("[STOMP]", str);
+        },
     });
 
     client.onConnect = onConnect;
-    client.onStompError = onError;
-    client.onWebSocketError = () => { };
+
+    client.onStompError = (frame) => {
+        console.error("STOMP error:", frame);
+        onError?.(frame);
+    };
+
+    client.onWebSocketError = (error) => {
+        console.error("WebSocket error:", error);
+    };
 
     return client;
 }
